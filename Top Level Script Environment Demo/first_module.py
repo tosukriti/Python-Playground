e13a360169169fcbc9754aa9ba70d88f812a9722
Sukriti Sarkar <tosukriti5@gmail.com>
2019-06-25 21:49:19 +0530

    adding files via upload

---
+++ Top Level Script Environment Demo/first_module.py
@@ -0,0 +1,7 @@
+# Helpful Link: https://docs.python.org/3/library/__main__.html
+if __name__ == "__main__":
+    # execute only if run as a script
+    print("Run Directly")
+else:
+	# execute this if first_module is run from second_module
+	print("Run from Import")
---
+++ Top Level Script Environment Demo/second_module.py
@@ -0,0 +1,2 @@
+# Importing first_module to check what will be the output when we will run this script
+import first_module
